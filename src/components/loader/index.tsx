27664b66ecbe0cbc355691b1d23e18cb46ae252b
tonildo123 <tony81191@gmail.com>
2026-05-14 22:50:51 -0300

    Loader OK

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { View, Animated, StyleSheet } from 'react-native';
+import { styles } from './styles';
 
+const SkeletonCard = () => {
+  const opacity = useRef(new Animated.Value(0.6)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(opacity, {
+          toValue: 1,
+          duration: 700,
+          useNativeDriver: true,
+        }),
+        Animated.timing(opacity, {
+          toValue: 0.6,
+          duration: 700,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, [opacity]);
+
+  return (
+    <Animated.View style={[styles.card, { opacity }]} />
+  );
+};
 
 const Loader = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <ActivityIndicator size="large" color="#0000ff" />
+  <View style={styles.container}>
+    {[...Array(6)].map((_, i) => (
+      <SkeletonCard key={i} />
+    ))}
   </View>
 );
+
+
 
 export default Loader;
---
+++ src/components/loader/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  card: {
+    height: 100,
+    borderRadius: 12,
+    backgroundColor: '#e0e0e0',
+    marginBottom: 16,
+  },
+});
